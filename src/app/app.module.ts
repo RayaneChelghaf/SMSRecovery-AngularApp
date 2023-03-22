28c82f69c9612be3d1f320396654dcce09e7e73d
2be-Rayane <125278584+2be-Rayane@users.noreply.github.com>
2023-03-22 15:54:11 +0100

    full conflict resolve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PostModule } from './post/post.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddFournisseurComponent } from './add-fournisseur/add-fournisseur.component';
+import { ListeFournisseursComponent } from './liste-fournisseurs/liste-fournisseurs.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AddFournisseurComponent,
+    ListeFournisseursComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PostModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
