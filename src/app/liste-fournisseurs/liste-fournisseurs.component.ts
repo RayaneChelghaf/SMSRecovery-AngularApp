a6a07342c9c4dbe1845642149e3f7f77b60fd51c
2be-Rayane <125278584+2be-Rayane@users.noreply.github.com>
2023-03-27 10:39:07 +0200

    test de modifications de pages pour reproduire le fonctionnement de Liste de messages sur Liste des fournisseurs

--- src/app/liste-fournisseurs/liste-fournisseurs.component.ts
+++ src/app/liste-fournisseurs/liste-fournisseurs.component.ts
@@ -2,9 +2,60 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-liste-fournisseurs',
-  templateUrl: './liste-fournisseurs.component.html',
-  styleUrls: ['./liste-fournisseurs.component.css']
+   templateUrl: './liste-fournisseurs.component.html',
+styleUrls: ['./liste-fournisseurs.component.css']
 })
 export class ListeFournisseursComponent {
 
 }
+
+
+
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { PostService } from '../post.service';
+// import { Post } from '../post';
+      
+// @Component({
+//   selector: 'app-liste-fournisseurs',
+//   templateUrl: './liste-fournisseurs.component.html',
+//   styleUrls: ['./liste-fournisseurs.component.css']
+// })
+// export class ListeFournisseursComponent implements OnInit {
+      
+//   posts: Post[] = [];
+    
+//   /*------------------------------------------
+//   --------------------------------------------
+//   Created constructor
+//   --------------------------------------------
+//   --------------------------------------------*/
+//   constructor(public postService: PostService) { }
+    
+//   /**
+//    * Write code on Method
+//    *
+//    * @return response()
+//    */
+//   ngOnInit(): void {
+//     this.postService.getAll().subscribe((data: Post[])=>{
+//       this.posts = data;
+//       console.log(this.posts);
+//     })  
+//   }
+    
+//   /**
+//    * Write code on Method
+//    *
+//    * @return response()
+//    */
+//   deletePost(id:number){
+//     this.postService.delete(id).subscribe(res => {
+//          this.posts = this.posts.filter(item => item.id !== id);
+//          console.log('Post deleted successfully!');
+//     })
+//   }
+    
+// }
--- src/app/post/post-routing.module.ts
+++ src/app/post/post-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListeFournisseursComponent } from '../liste-fournisseurs/liste-fournisseurs.component';
 import { IndexComponent } from './index/index.component';
+
 import { ViewComponent } from './view/view.component';
 import { CreateComponent } from './create/create.component';
 import { EditComponent } from './edit/edit.component';
@@ -8,11 +10,12 @@ import { EditComponent } from './edit/edit.component';
 const routes: Routes = [
   { path: 'message', redirectTo: 'message/index', pathMatch: 'full'},
   { path: 'message/index', component: IndexComponent },
+  { path: 'message/liste-fournisseurs', component: ListeFournisseursComponent },
+
   { path: 'message/:messageId/view', component: ViewComponent },
   { path: 'message/create', component: CreateComponent },
   { path: 'message/:messageId/edit', component: EditComponent } 
 ];
-  
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
