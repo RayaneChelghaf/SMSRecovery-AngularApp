d9635f9384fa54d7d27011e00111ba3f922f7b80
2be-Rayane <125278584+2be-Rayane@users.noreply.github.com>
2023-03-29 10:52:19 +0200

    new testing branch for action tools development

---
+++ src/app/fournisseurs/post-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ListeFournisseursComponent } from '../liste-fournisseurs/liste-fournisseurs.component';
+import { IndexComponent } from './index/index.component';
+
+import { ViewComponent } from '../liste-fournisseurs/view/view.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+
+const routes: Routes = [
+  { path: 'liste-fournisseurs', redirectTo: 'liste-fournisseurs/index', pathMatch: 'full'},
+  { path: 'liste-fournisseurs/index', component: IndexComponent },
+  { path: 'message/liste-fournisseurs', component: ListeFournisseursComponent },
+
+  { path: 'liste-fournisseurs/:messageId/view', component: ViewComponent },
+  { path: 'liste-fournisseurs/create', component: CreateComponent },
+  { path: 'liste-fournisseurs/:messageId/edit', component: EditComponent }
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PostRoutingModule { }
