3057f2ffb9777f1fcf4be9f72efbb0ec11815117
2be-Rayane <125278584+2be-Rayane@users.noreply.github.com>
2023-04-03 17:02:11 +0200

    affichage des noms des fournisseurs + placeholder device et libellé

--- src/app/devices/liste-devices.component.ts
+++ src/app/devices/liste-devices.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProviderService} from "../services/provider.service";
+import {ProviderModel} from "../add-fournisseur/add-fournisseur.component";
+import {tap} from "rxjs";
 
 @Component({
-  selector: 'app-liste-devices',
-  templateUrl: './liste-devices.component.html',
-  styleUrls: ['./liste-devices.component.css']
+  selector: 'app-liste-fournisseur',
+   templateUrl: './liste-devices.component.html',
+styleUrls: ['./liste-devices.component.css']
 })
-export class ListeDevicesComponent {
+export class ListeDevicesComponent implements OnInit{
+
+  public items : ProviderModel[] | null = null;
+
+  constructor(private provider : ProviderService) {
+
+  }
+
+  ngOnInit(): void {
+
+    let result = this.provider.list();
+
+    result
+      .pipe(
+        tap(t => this.items = t)
+      )
+      .subscribe();
+
+  }
 
 }
