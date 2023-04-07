78cd416b06f80d6769069085eb4ade52a8af3091
2be-Rayane <125278584+2be-Rayane@users.noreply.github.com>
2023-04-07 15:37:05 +0200

    changes for button testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,3 +11,55 @@ import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/fo
 export class AppComponent {
   title = 'SMSRecovery-AngularApp';
 }
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { DialogService } from './services/dialog.service';
+// import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+// // import Validation from './utils/validation';
+
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css'],
+// })
+// export class AppComponent {
+//   title = 'SMSRecovery-AngularApp';
+
+//   constructor(private dialog: DialogService) {}
+
+//   yesNoDialog() {
+//     this.dialog
+//       .confirmDialog({
+//         title: 'Are you sure?',
+//         message: 'Are you sure you want to do this?',
+//         confirmCaption: 'Yes',
+//         cancelCaption: 'No',
+//       })
+//       .subscribe((yes) => {
+//         if (yes) console.log('The user said YES');
+//       });
+//   }
+
+//   confirmCancelDialog() {
+//     this.dialog
+//       .confirmDialog({
+//         title: 'Confirm Action',
+//         message: 'Do you want to confirm this action?',
+//         confirmCaption: 'Confirm',
+//         cancelCaption: 'Cancel',
+//       })
+//       .subscribe((confirmed) => {
+//         if (confirmed) console.log('The user confirmed the action');
+//       });
+//   }
+// }
