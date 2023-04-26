2ac475ba6e5fb5a796d1df5f1a0d76d240fee16d
2be-Rayane <125278584+2be-Rayane@users.noreply.github.com>
2023-04-26 17:00:21 +0200

    test imports pour filte tableau

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,29 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-
 import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-
-import {NavbarComponent} from './navbar/navbar.component';
-import {AddFournisseurComponent} from './add-fournisseur/add-fournisseur.component';
-import {ListeFournisseursComponent} from './items/liste-fournisseurs/index/liste-fournisseurs.component';
-import {ListeDevicesComponent} from './devices/liste-devices.component';
 import {FormsModule} from "@angular/forms";
 import {CommonModule} from "@angular/common";
-import {ViewComponent} from "./items/liste-fournisseurs/view/view.component";
-import {EditComponent} from "./items/liste-fournisseurs/edit/edit.component";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-import {ViewDeviceComponent} from "./devices/view/view.component";
-
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+
+import {AppComponent} from './app.component';
+import {ViewDeviceComponent} from "./devices/view/view.component";
 import {DeviceComponent} from './message/message.component';
 import {AddDeviceComponent} from "./devices/add-device/add-device.component";
+import {NavbarComponent} from './navbar/navbar.component';
+import {ViewComponent} from "./items/liste-fournisseurs/view/view.component";
+import {EditComponent} from "./items/liste-fournisseurs/edit/edit.component";
+import {AddFournisseurComponent} from './add-fournisseur/add-fournisseur.component';
+import {ListeFournisseursComponent} from './items/liste-fournisseurs/index/liste-fournisseurs.component';
+import {ListeDevicesComponent} from './devices/liste-devices.component';
+
+
 
 @NgModule({
     declarations: [
@@ -51,7 +51,8 @@ import {AddDeviceComponent} from "./devices/add-device/add-device.component";
         MatIconModule,
         MatButtonModule,
         MatCardModule,
-        MatInputModule
+        MatInputModule,
+        MatSortModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -29,3 +29,48 @@ export interface Message {
     deviceId: string;
     label: string;
 }
+
+
+
+// import { Component, OnInit, ViewChild } from '@angular/core';
+// import { MatSort, Sort } from '@angular/material/sort';
+// import { ProviderService } from '../services/provider.service';
+// import { tap } from 'rxjs/operators';
+
+// @Component({
+//   selector: 'app-message',
+//   templateUrl: './message.component.html',
+//   styleUrls: ['./message.component.css'],
+// })
+// export class DeviceComponent implements OnInit {
+//   public messages: Message[] | null = null;
+//   displayedColumns: string[] = ['date', 'address', 'body', 'label', 'deviceId'];
+//   @ViewChild(MatSort) sort: MatSort
+// //    | undefined
+// ;
+
+//   constructor(private providerService: ProviderService) {}
+
+//   ngOnInit(): void {
+//     this.providerService
+//       .listMessages()
+//       .pipe(tap((t) => (this.messages = t)))
+//       .subscribe();
+//   }
+
+//   applyFilter(filterValue: string) {
+//     if (this.messages) {
+//       this.messages = this.messages.filter((message) =>
+//         message.body.toLowerCase().includes(filterValue.toLowerCase())
+//       );
+//     }
+//   }
+// }
+
+// export interface Message {
+//   address: string;
+//   body: string;
+//   date: Date;
+//   deviceId: string;
+//   label: string;
+// }
