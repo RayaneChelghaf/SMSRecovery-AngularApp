7ba9ced060acc6806548d42ec3d873500a67fe41
2be-Rayane <125278584+2be-Rayane@users.noreply.github.com>
2023-05-12 12:12:35 +0200

    ajout de query filter dans l'url. depuis l'accueil on peut désormais redirect via query

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,109 +1,74 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {ProviderService} from "../services/provider.service";
-import {tap} from "rxjs";
-import {MatTableDataSource} from "@angular/material/table";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatSort} from "@angular/material/sort";
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { ProviderService } from "../services/provider.service";
+import { tap } from "rxjs";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-    selector: 'app-message',
-    templateUrl: './message.component.html',
-    styleUrls: ['./message.component.css']
+  selector: 'app-message',
+  templateUrl: './message.component.html',
+  styleUrls: ['./message.component.css']
 })
 export class DeviceComponent implements OnInit, AfterViewInit {
 
-    public messages: Message[] | null = null;
+  public messages: Message[] | null = null;
 
-   dataSource = new MatTableDataSource<Message>([]);
-   displayedColumns = ["address", "body", "date", "deviceId", "label"];
+  dataSource = new MatTableDataSource<Message>([]);
+  displayedColumns = ["address", "body", "date", "deviceId", "label"];
+  filter: string = '';
 
-    @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
-    @ViewChild(MatSort) sort: MatSort | null = null;
+  @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+  @ViewChild(MatSort) sort: MatSort | null = null;
 
+  constructor(private providerService: ProviderService, private router: Router, private route: ActivatedRoute) {
+  }
 
-    constructor(private providerService: ProviderService) {
+  ngAfterViewInit(): void {
+    this.reconnectSortAndPaginator();
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.filter = filterValue.trim().toLowerCase();
+    const queryParams = { q: this.filter };
+    this.router.navigate([], { queryParams });
+    this.dataSource.filter = this.filter;
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
     }
+  }
 
-    ngAfterViewInit(): void {
-        this.reconnectSortAndPaginator();
-    }
-    
-    applyFilter(event: Event) {
-        const filterValue = (event.target as HTMLInputElement).value;
-        this.dataSource.filter = filterValue.trim().toLowerCase();
-    
-        if (this.dataSource.paginator) {
-          this.dataSource.paginator.firstPage();
-        }
-      }
-    private reconnectSortAndPaginator() {
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-    }
+  private reconnectSortAndPaginator() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
 
-    ngOnInit(): void {
-        this.providerService.listMessages()
-            .pipe(
-                tap(t => this.messages = t),
-                tap(t => {
-                    if (this.messages && this.messages.length ) {
-                        this.dataSource = new MatTableDataSource<Message>(this.messages)  ;
-                        this.reconnectSortAndPaginator();
-                    }
-                })
-            ).subscribe();
-    }
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe(params => {
+      this.filter = params.get('q') || '';
+      this.dataSource.filter = this.filter;
+    });
+
+    this.providerService.listMessages()
+      .pipe(
+        tap(t => this.messages = t),
+        tap(t => {
+          if (this.messages && this.messages.length) {
+            this.dataSource = new MatTableDataSource<Message>(this.messages);
+            this.reconnectSortAndPaginator();
+          }
+        })
+      ).subscribe();
+  }
 }
 
 export interface Message {
-    address: string;
-    body: string;
-    date: Date;
-    deviceId: string;
-    label: string;
+  address: string;
+  body: string;
+  date: Date;
+  deviceId: string;
+  label: string;
 }
-
-
-
-// import { Component, OnInit, ViewChild } from '@angular/core';
-// import { MatSort, Sort } from '@angular/material/sort';
-// import { ProviderService } from '../services/provider.service';
-// import { tap } from 'rxjs/operators';
-
-// @Component({
-//   selector: 'app-message',
-//   templateUrl: './message.component.html',
-//   styleUrls: ['./message.component.css'],
-// })
-// export class DeviceComponent implements OnInit {
-//   public messages: Message[] | null = null;
-//   displayedColumns: string[] = ['date', 'address', 'body', 'label', 'deviceId'];
-//   @ViewChild(MatSort) sort: MatSort
-// //    | undefined
-// ;
-
-//   constructor(private providerService: ProviderService) {}
-
-//   ngOnInit(): void {
-//     this.providerService
-//       .listMessages()
-//       .pipe(tap((t) => (this.messages = t)))
-//       .subscribe();
-//   }
-
-//   applyFilter(filterValue: string) {
-//     if (this.messages) {
-//       this.messages = this.messages.filter((message) =>
-//         message.body.toLowerCase().includes(filterValue.toLowerCase())
-//       );
-//     }
-//   }
-// }
-
-// export interface Message {
-//   address: string;
-//   body: string;
-//   date: Date;
-//   deviceId: string;
-//   label: string;
-// }
